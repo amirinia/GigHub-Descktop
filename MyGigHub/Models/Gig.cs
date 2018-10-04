31b10c18f88b6aefea227816f4fd079847308567
Hossein amirinia <amirynia@yahoo.com>
2018-10-04 17:56:45 +0330

    commit

---
+++ MyGigHub/Models/Gig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyGigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Artist { get; set; }
+        [Required]
+        public string ArtistId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public String Venue { get; set; }
+
+        public Genre Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
---
+++ MyGigHub/Models/Timesheet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyGigHub.Models
+{
+    public class Timesheet
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Artist { get; set; }
+        [Required]
+        public string ArtistId { get; set; }
+
+        public DateTime StartDay { get; set; }
+
+        public DateTime EndDay { get; set; }
+
+        public TimeSpan GetDifferenceTimes()
+        {
+            return StartDay - EndDay;
+        }
+    }
+}
